4f3b137635b76a2b494cabcb5b174154a35ffa16
sevketugurel <ugurelsevket@gmail.com>
2023-06-02 18:34:54 +0300

    Initial Commit

---
+++ FotografPaylasmaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotografPaylasmaUygulamasi
+//
+//  Created by Şevket Uğurel on 2.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
